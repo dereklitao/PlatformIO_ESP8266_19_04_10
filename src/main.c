27f6011f00878bf112ff1cb5d9f8c096d512a290
dereklitao <dereklitao@outlook.com>
2019-04-11 22:57:31 +0800

    test

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@ void app_main(void)
 
     nvs_flash_init();
 
+    printf("\r\nHello github\r\n");
+
     xTaskCreate(test_task, "test_task", 4096, NULL, 10, NULL);
     xTaskCreate(csro_smart_config_task, "csro_smart_config_task", 4096, NULL, 10, NULL);
 }
